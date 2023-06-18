b878340cf624d664bf081c043e25bcc4b2a41391
mr1mokhtari <mr1mokhtari@gmail.com>
2023-06-18 21:39:09 +0430

    5.5-6 Bloglist Frontend, Togglable new blog form in separate components

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,17 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Blog from './components/Blog';
 import blogService from './services/blogs';
 import loginService from './services/login';
 import LoginForm from './components/LoginForm';
 import BlogForm from './components/BlogForm';
 import Notification from './components/Notification';
-
+import Togglable from './components/Togglable';
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [message, setMessage] = useState();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+
+  const blogFormRef = useRef();
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -56,6 +58,7 @@ const App = () => {
   };
 
   const createBlog = async (blogObject) => {
+    blogFormRef.current.toggleVisibility();
     try {
       const newBlog = await blogService.create(blogObject);
       setBlogs(blogs.concat(newBlog));
@@ -89,7 +92,9 @@ const App = () => {
             {`${user.name} logged in `}
             <button onClick={handleLogout}>Logout</button>
           </p>
-          <BlogForm createBlog={createBlog} />
+          <Togglable buttonLabel="new blog" ref={blogFormRef}>
+            <BlogForm createBlog={createBlog} />
+          </Togglable>
 
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
